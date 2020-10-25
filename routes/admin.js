0e3e98d48f4b5e1e6084cc49cdd6b8e10634fc44
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-25 06:52:49 +0200

    feat(Node): Add path

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { docTitle: 'Add Product Page' });
+    res.render('add-product', { pageTitle: 'Add Product Page', path: '/add-product' });
 });
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('shop', { prods: adminData.products, docTitle: 'Shop' });
+    res.render('shop', { prods: adminData.products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
