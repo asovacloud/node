04501a1406825b2ab3456078ecea2d375042a410
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-25 23:06:08 +0200

    Refactoring code

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const path = require('path');
-
-const rootDir = require('../util/path');
 
 const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product Page', path: '/add-product', activeProduct: true });
+    res.render(
+        'add-product', {
+            pageTitle: 'Add Product Page',
+            path: '/add-product'
+        });
 });
 
 router.post('/add-product', (req, res, next) => {
