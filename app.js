6cc85d800174d38f7426958d56d12dbe7d9bb471
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-25 22:51:15 +0200

    feat(Add Product): Add ejs

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const exphbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('.hbs', exphbs({
-    extname: '.hbs',
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout'
-}));
-app.set('view engine', '.hbs');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
