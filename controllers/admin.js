73a78c9da096fc5fca8d3bae279056e1d5d9c0c2
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-29 11:50:51 +0200

    feat(Add Product): Fetching Admin Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,15 +65,17 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render(
-            'admin/products', {
-                prods: products,
-                pageTitle: 'Admin Products',
-                path: '/admin/products'
-            }
-        );
-    });
+    Product.findAll()
+        .then(prods => {
+            res.render(
+                'admin/products', {
+                    prods,
+                    pageTitle: 'Admin Products',
+                    path: '/admin/products'
+                }
+            );
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
