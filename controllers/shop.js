e579b1ec35ffb50a84389a3dc03e7aae3dc8dda5
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-30 20:36:37 +0200

    feat(Add Product): Resetting the Cart & Fetching and Outputting orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -106,8 +106,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+
     req.user.getCart()
         .then(cart => {
+            fetchedCart = cart;
             return cart.getProducts();
         })
         .then(products => {
@@ -121,21 +124,24 @@ exports.postOrder = (req, res, next) => {
                 .catch(err => console.log(err));
         })
         .then(result => {
+            return fetchedCart.setProducts(null);
+        })
+        .then(result => {
             res.redirect('/orders');
         })
         .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        pageTitle: 'Your Orders',
-        path: '/orders'
-    });
-};
-
-exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        pageTitle: 'Checkout',
-        path: '/checkout'
-    });
+    req.user.getOrders({
+            include: ['products']
+        })
+        .then(orders => {
+            res.render('shop/orders', {
+                pageTitle: 'Your Orders',
+                path: '/orders',
+                orders
+            });
+        })
+        .catch(err => console.log(err))
 };
