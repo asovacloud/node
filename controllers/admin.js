1ba7f193583ec83b28015f17465323493e7a7e69
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-30 15:59:18 +0200

    feat(Add Product): Fetching Related Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -35,8 +35,11 @@ exports.getEditProduct = (req, res, next) => {
     }
     const prodId = req.params.productId;
 
-    Product.findByPk(prodId)
-        .then(product => {
+    req.user.getProducts({ where: { id: prodId } })
+        // Product.findByPk(prodId)
+        .then(products => {
+            const product = products[0];
+
             if (!product) {
                 return res.redirect('/');
             }
@@ -73,7 +76,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll()
+    req.user
+        .getProducts()
         .then(prods => {
             res.render(
                 'admin/products', {
