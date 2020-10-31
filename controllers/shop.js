769984e3860cc48eedb804bbb70b7dd49151a9fa
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-31 17:38:49 +0200

    feat(Add Product): Fetching a Single Product

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,14 +16,15 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
 
-    Product.findByPk(prodId)
+    Product.findById(prodId)
         .then(product => {
             res.render('shop/product-detail', {
                 product,
                 pageTitle: product.title,
                 path: '/products'
             });
-        });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
