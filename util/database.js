ecd8e1ff857ca6f5be8dc3a4834279442ec40375
Andrew Sova <andrii.sova@tbwa-x.com>
2020-10-31 16:27:22 +0200

    feat(Add Product): Creating the Database Connection

--- util/database.js
+++ util/database.js
@@ -1,16 +1,28 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://asova:nodecomplete@cluster0.pv5vz.mongodb.net/test?retryWrites=true&w=majority";
+
+const uri = "mongodb+srv://asova:nodecomplete@cluster0.pv5vz.mongodb.net/shop?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+let _db;
 
 const mongoConnect = (callback) => {
     client.connect()
         .then(client => {
-            console.log('=========');
-            console.log('Result: ', client);
-            console.log('Was connected');
-            callback(client);
+            console.log('Connected!');
+            _db = client.db();
+            callback();
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
