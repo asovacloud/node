91dfe09109eacdf6adb31f48a58197872a5068f2
Andrew Sova <andrii.sova@tbwa-x.com>
2020-11-08 21:52:50 +0200

    feat(Shop): Using Nodemailar for Send an Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(
+    sendgridTransport({
+        auth: {
+            api_key: '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'
+        }
+    })
+);
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -82,6 +92,15 @@ exports.postSignup = (req, res, next) => {
                 })
                 .then(result => {
                     res.redirect('/login');
+                    return transporter.sendMail({
+                        to: email,
+                        from: 'asovacloud@gmail.com',
+                        subject: 'Signup succeeded!',
+                        html: '<h1>You successfully signed up!</h1>'
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
                 });
         })
         .catch(err => console.log(err));
