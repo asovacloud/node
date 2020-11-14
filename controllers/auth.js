df5369c37fd6d7fda142b25390e5b823317defd2
Andrew Sova <asovacloud@gmail.com>
2020-11-14 21:15:22 +0200

    feat(Shop): Adding Async Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -75,7 +75,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
+    
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log(errors.array()[0].msg);
@@ -85,14 +85,8 @@ exports.postSignup = (req, res, next) => {
             errorMessage: errors.array()[0].msg
         });
     }
-    User.findOne({ email })
-        .then(userDoc => {
-            if (userDoc) {
-                req.flash('error', 'E-Mail exists already, please pick a different one.')
-                return res.redirect('/signup');
-            }
-            return bcrypt
-                .hash(password, 12)
+    bcrypt
+            .hash(password, 12)
                 .then(password => {
                     const user = new User({
                         email,
@@ -113,8 +107,6 @@ exports.postSignup = (req, res, next) => {
                 .catch(err => {
                     console.log(err);
                 });
-        })
-        .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -14,11 +15,22 @@ router.post('/login', authController.postLogin);
 router.post(
     '/signup',
     [
-        check('email').isEmail().withMessage('Please enter a valid email.').custom((value, { req }) => {
-            if (value === 'test@test.com') {
-                throw new Error('This email address is forbidden.');
-            }
-            return true;
+        check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email.')
+        .custom((value, { req }) => {
+            // if (value === 'test@test.com') {
+            //     throw new Error('This email address is forbidden.');
+            // }
+            // return true;
+            return User.findOne({ email: value })
+            .then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject(
+                        'E-Mail exists alread, please pick a different one.'
+                    );
+                }
+            });
         }),
         body(
             'password',
