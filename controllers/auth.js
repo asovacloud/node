d5ba9489b9fb196ee167509e3bc512a55d80c4db
Andrew Sova <asovacloud@gmail.com>
2020-11-15 21:47:10 +0200

    feat(Shop): Keeping User Input

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,7 +38,12 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         pageTitle: 'Signup',
         path: '/signup',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
     });
 };
 
@@ -47,7 +52,7 @@ exports.postLogin = (req, res, next) => {
     const password = req.body.password;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.render('auth/login', {
+        return res.status(422).render('auth/login', {
             path: '/login',
             pageTitle: 'Login',
             errorMessage: errors.array()[0].msg
@@ -92,7 +97,12 @@ exports.postSignup = (req, res, next) => {
         return res.status(422).render('auth/signup', {
             path: '/signup',
             pageTitle: 'Signup',
-            errorMessage: errors.array()[0].msg
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email,
+                password,
+                confirmPassword: req.body.confirmPassword
+            }
         });
     }
     bcrypt
